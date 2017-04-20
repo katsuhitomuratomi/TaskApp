7ba17630ceacb07de913b064a69e3980977223b0
katsuhitomuratomi <garo0713@icloud.com>
2017-04-20 22:40:23 +0900

    fin

--- app/src/main/java/jp/techacademy/katsuhito/muratomi/taskapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/katsuhito/muratomi/taskapp/MainActivity.java
@@ -1,14 +1,18 @@
 package jp.techacademy.katsuhito.muratomi.taskapp;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
-
-import java.util.Date;
 
 import io.realm.Realm;
 import io.realm.RealmChangeListener;
@@ -16,6 +20,7 @@ import io.realm.RealmResults;
 import io.realm.Sort;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_TASK="jp.techacademy.katsuhito.muratomi.taskapp.TASK";
 
     private Realm mRealm;
     private RealmChangeListener mRealmListener = new RealmChangeListener() {
@@ -24,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
             reloadListView();
         }
     };
+    EditText editText;
+    Button button;
+    String searchstring;
 
     private ListView mListView;
     private TaskAdapter mTaskAdapter;
@@ -33,15 +41,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-
+                Intent intent = new Intent(MainActivity.this, InputActivity.class);
+                startActivity(intent);
             }
         });
+        editText =(EditText)findViewById(R.id.search_edit_text);
+        button=(Button)findViewById(R.id.sbutton);
+
 
         //Realmの設定
         mRealm = Realm.getDefaultInstance();
@@ -55,20 +66,78 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //入力、編集する画面に遷移させる
+                Task task = (Task) parent.getAdapter().getItem(position);
+
+                Intent intent = new Intent(MainActivity.this, InputActivity.class);
+                intent.putExtra(EXTRA_TASK, task.getId());
+
+                startActivity(intent);
             }
         });
 
         mListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                //タスクを削除する
+                // タスクを削除する
+
+                final Task task = (Task) parent.getAdapter().getItem(position);
+
+                // ダイアログを表示する
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+                builder.setTitle("削除");
+                builder.setMessage(task.getTitle() + "を削除しますか");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        RealmResults<Task> results = mRealm.where(Task.class).equalTo("id", task.getId()).findAll();
+
+                        mRealm.beginTransaction();
+                        results.deleteAllFromRealm();
+                        mRealm.commitTransaction();
+
+                        Intent resultIntent=new Intent(getApplicationContext(),TaskAlarmReceiver.class);
+                        PendingIntent resultPedingIntent=PendingIntent.getBroadcast(
+                                MainActivity.this,
+                                task.getId(),
+                                resultIntent,
+                                PendingIntent.FLAG_UPDATE_CURRENT
+                        );
+                        AlarmManager alarmManager=(AlarmManager)getSystemService(ALARM_SERVICE);
+                        alarmManager.cancel(resultPedingIntent);
+
+                        reloadListView();
+                    }
+                });
+                builder.setNegativeButton("CANCEL", null);
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
                 return true;
             }
         });
-        addTaskForTest();
+
         reloadListView();
 
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                searchstring=editText.getText().toString();
+                if(searchstring.length()==0){
+                    reloadListView();
 
+                }else{
+                    RealmResults<Task> categoryresults = mRealm.where(Task.class).equalTo("category",searchstring).findAll();
+                    mTaskAdapter.setTaskList(mRealm.copyFromRealm(categoryresults));
+                    mListView.setAdapter(mTaskAdapter);
+                    mTaskAdapter.notifyDataSetChanged();
+
+                }
+
+            }
+        });
     }
 
     private void reloadListView() {
@@ -84,16 +153,6 @@ public class MainActivity extends AppCompatActivity {
         mRealm.close();
     }
 
-    private void addTaskForTest() {
-        Task task = new Task();
-        task.setTitle("作業");
-        task.setContents("プログラムを書いてPUSHする");
-        task.setDate(new Date());
-        task.setId(0);
-        mRealm.beginTransaction();
-        mRealm.copyToRealmOrUpdate(task);
-        mRealm.commitTransaction();
 
-    }
 
 }
